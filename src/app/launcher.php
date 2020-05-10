10ffb02793aa9e4ff3f37c5b000929f1711028a6
Dave Bray <david@brayworth.com.au>
2020-05-10 16:05:56 +1000

    dev

--- src/app/launcher.php
+++ src/app/launcher.php
@@ -24,23 +24,33 @@ class launcher {
 			new dvc\application( dirname( __DIR__));
 
 		}
-		elseif ( class_exists('Parsedown')) {
-			/**
-			 * Well not that simple - you have extended it with
-			 * composer require erusev/parsedown
-			 */
-
-			print '<html><body>';
-			print Parsedown::instance()->text( file_get_contents( __DIR__ . '/../../Readme.md'));
-			print '</body></html>';
-
-		}
 		else {
+			if ( class_exists('pages\page')) {
+				$page = new pages\page;  // from outside this namespace
+				$page->open();
 
-			/**
-			 * Yeah - the Minimum Viable Product
-			 */
-			print 'hello world';
+			}
+
+			if ( class_exists('Parsedown')) {
+				/**
+				 * Well not that simple - you have extended it with
+				 * composer require erusev/parsedown
+				 */
+
+				print '<html><body>';
+				print Parsedown::instance()->text( file_get_contents( __DIR__ . '/../../Readme.md'));
+				print '</body></html>';
+
+			}
+			else {
+
+				/**
+				 * Yeah - the Minimum Viable Product
+				 */
+				header( 'content/type: text/plain');
+				print 'hello world';
+
+			}
 
 		}
 
